8c03393e7c33395741ec1cedc0270b643b23718b
Oghenefejiro Ephraim Esosuota <ephraim2039@gmail.com>
2025-07-20 23:25:43 +0000

    Added Models.py

---
+++ server-side/models.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Column, Integer, Float, String, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+from datetime import datetime
+
+Base = declarative_base()
+
+class PaymentRecord(Base):
+    __tablename__ = "payment_records"
+
+    id = Column(Integer, primary_key=True, index=True)
+    cost = Column(Float, nullable=False)
+    revenue = Column(Float, nullable=False)
+    time_horizon = Column(Integer, nullable=False)
+    roi_percent = Column(String, nullable=False)
+    break_even_months = Column(String, nullable=False)
